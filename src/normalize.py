56510b76bb414563086892145be493079e7197ef
Guido <me@guido.io>
2016-05-16 13:02:21 +0200

    Normalization

---
+++ src/normalize.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+MIN_BOUND = -1000
+MAX_BOUND = 400
+
+def normalize(image):
+    image = (image - MIN_BOUND) / (MAX_BOUND - MIN_BOUND)
+    image[image>1] = 1.
+    image[image<0] = 0.
+    return image
