a36173b615f78f86dbfb49ab0f50613baf2eb3c0
Guido <me@guido.io>
2016-05-16 15:38:12 +0200

    Merging as data frames

--- src/candidates.py
+++ src/candidates.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 
 #Merge candidates of a single scan
-def merge_candidates_scan(candidates, distance=5.):
+def merge_candidates_scan(candidates, seriesuid, distance=5.):
     distances = pdist(candidates, metric='euclidean')
     adjacency_matrix = squareform(distances)
 
@@ -16,18 +16,28 @@ def merge_candidates_scan(candidates, distance=5.):
 
     # Determine all connected components in the graph
     n, labels = connected_components(adjacency_matrix)
-
     new_candidates = np.zeros((n,3))
 
     # Take the mean for these connected components
     for cluster_i in range(n):
         points = candidates[np.where(labels==cluster_i)]
-        new_candidates[cluster_i,:] = np.mean(points,axis=0)
+        center = np.mean(points,axis=0)
+        new_candidates[cluster_i,:] = center
+
+    x = new_candidates[:,0]
+    y = new_candidates[:,1]
+    z = new_candidates[:,2]
+    labels = [seriesuid]*len(x)
+    class_name = [0]*len(x)
+
+    data= zip(labels,x,y,z,class_name)
+
+    new_candidates = pd.DataFrame(data,columns=['seriesuid','coordX','coordY','coordZ','class'])
 
     return new_candidates
 
 def merge_candidates(df_candidates, distance=5.):
-    new_candidates = np.zeros((0,3))
+    new_candidates = pd.DataFrame()
     for scan_name in tqdm(df_candidates['seriesuid'].unique()):
         #print "Merging scan", scan_name
         df = df_candidates[df_candidates['seriesuid']==scan_name]
@@ -40,10 +50,12 @@ def merge_candidates(df_candidates, distance=5.):
         candidates[:,1]=y
         candidates[:,2]=z
 
-        new = merge_candidates_scan(candidates, distance)
-        new_candidates = np.append(new_candidates, new,axis=0)
+        new = merge_candidates_scan(candidates,seriesuid=scan_name,distance=distance)
+        new_candidates = new_candidates.append(new)
 
-    print new_candidates
+
+    #print new_candidates
+    return new_candidates
 
 
 def load_candidates(filename, as_coords=False):
@@ -56,8 +68,10 @@ def load_candidates(filename, as_coords=False):
 # seriesuid, coordX, coordY, coordZ, class
 # class seems to be 0 always
 def save_candidates(filename, df_candidates):
-    pass
+    df_candidates.to_csv(filename, index=False)
 
 if __name__ == "__main__":
+
     df = load_candidates('../data/candidates.csv')
-    merge_candidates(df)
+    new_candidates = merge_candidates(df)
+    save_candidates('test.csv', new_candidates)
