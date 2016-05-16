354c8272ce6f649f46f8df6b10fb7c9233eea9b3
robbertvdg <robbert.gugten@student.ru.nl>
2016-05-16 13:33:18 +0200

    added z-coordinate

--- src/evaluate_candidates.py
+++ src/evaluate_candidates.py
@@ -7,8 +7,10 @@ def check_coordinates(image_coord,candidate):
     diameter = candidate[4]
     coord_x = candidate[1]
     coord_y = candidate[2]
+    coord_z = candidate[3]
     if image_coord[0] > coord_x - 0.5*diameter and image_coord[0] < coord_x + 0.5*diameter and\
-       image_coord[1] > coord_y - 0.5*diameter and image_coord[1] < coord_y + 0.5*diameter:
+       image_coord[1] > coord_y - 0.5*diameter and image_coord[1] < coord_y + 0.5*diameter and\
+       image_coord[2] > coord_z - 0.5*diameter and image_coord[2] < coord_y + 0.5*diameter:
         return True
     return False
 
@@ -25,7 +27,7 @@ def is_candidate(image_coord,image_name):
 if __name__ == "__main__":
     global candidates
     candidates = image_read_write.load_candidates("../data/annotations/annotations.csv")
-    print is_candidate([200,-177],"1.3.6.1.4.1.14519.5.2.1.6279.6001.100225287222365663678666836860")
+    print is_candidate([-130,-177,-299],"1.3.6.1.4.1.14519.5.2.1.6279.6001.100225287222365663678666836860")
 
 
 
