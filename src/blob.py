91413515fe6c69becbba807de2b88cb20987c72d
Michael <m.dekaste@student.ru.nl>
2016-05-23 21:00:36 +0200

    updated functions

--- src/blob.py
+++ src/blob.py
@@ -2,6 +2,7 @@ import SimpleITK as sitk
 import numpy as np
 import normalize as norm
 from skimage import filter as filters
+from joblib import Parallel
 from skimage import exposure
 from skimage import feature
 import matplotlib.pyplot as plt
@@ -38,6 +39,70 @@ def label_image(image):
     blobs = image > image.mean()
     return blobs
 
+def blob_image_multiscale2(image, type=0):
+    # function that return a list of blob_coordinates, 0 = dog, 1 = doh, 2 = log
+    list = []
+    image = norm.normalize(image)
+    for z, slice in tqdm(enumerate(image)):
+        # init list of different sigma/zoom blobs
+        featureblobs = []
+        # x = 0,1,2,3,4
+        for x in xrange(5):
+            if type == 0:
+                featureblobs[x] = feature.blob_dog(slice, 2^x, 2^(x+1))
+            if type == 1:
+                featureblobs[x] = feature.blob_doh(slice, 2^x, 2^(x+1))
+            if type == 2:
+                featureblobs[x] = feature.blob_log(slice, 2^x, 2^(x+1))
+        # init list of blob coords
+        blob_coords = np.zeros((len(featureblobs[0]),4))
+        # start at biggest blob size
+        for featureblob in reversed(featureblobs):
+            i = 0
+            # for every blob found of a blobsize
+            for blob in enumerate(featureblob):
+                # if that blob is not within range of another blob, add it
+                if(not within_range(blob, blob_coords)):
+                    blob_coords[i] = [z, blob[0], blob[1], blob[2]]
+                    i = i + 1
+        list.append(blob_coords[0:3])
+    return list
+
+def blob_image_multiscale3(image, type=0):
+    # function that return a list of blob_coordinates, 0 = dog, 1 = doh, 2 = log
+    list = []
+    image = norm.normalize(image)
+    for z, slice in tqdm(enumerate(image)):
+        # init list of different sigma/zoom blobs
+        featureblobs = []
+        # x = 0,1,2
+        for x in xrange(3):
+            if type == 0:
+                featureblobs[x] = feature.blob_dog(slice, 3^x, 3^(x+1))
+            if type == 1:
+                featureblobs[x] = feature.blob_doh(slice, 3^x, 3^(x+1))
+            if type == 2:
+                featureblobs[x] = feature.blob_log(slice, 3^x, 3^(x+1))
+        # init list of blob coords
+        blob_coords = np.zeros((len(featureblobs[0]),4))
+        # start at biggest blob size
+        for featureblob in reversed(featureblobs):
+            i = 0
+            # for every blob found of a blobsize
+            for blob in enumerate(featureblob):
+                # if that blob is not within range of another blob, add it
+                if not within_range(blob, blob_coords):
+                    blob_coords[i] = [z, blob[0], blob[1], blob[2]]
+                    i = i + 1
+        list.append(blob_coords[0:3])
+    return list
+
+def within_range(blob, blob_coords):
+    for coords in blob_coords:
+        if((blob[0] - coords[1])^2 + (blob[1] - coords[2]) < coords[3]^2):
+            return 1
+    return 0
+
 def blob_image(image):
     #img_path    =   '../1.3.6.1.4.1.14519.5.2.1.6279.6001.100332161840553388986847034053.mhd'
     # slice = numpy_image[240,:,:]
@@ -55,7 +120,6 @@ def blob_image(image):
     for z, slice in tqdm(enumerate(image)):
         blobs = feature.blob_doh(slice)
         #print blobs.shape
-        #print blobs
         blob_coords = np.zeros((len(blobs),3))
         for i, blob in enumerate(blobs):
             blob_coords[i] = [z, blob[0], blob[1]]
