f5fe988ab9c58aa02ccb19c3d205256d36a9818d
Guido <me@guido.io>
2016-06-01 17:00:14 +0200

    Theano flags, walltime

--- scripts/test_slurm.sh
+++ scripts/test_slurm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#SBATCH -t 1:00:00
 #SBATCH -n 4
 #SBATCH -p gpu_short
 
@@ -9,5 +10,6 @@ module load python/2.7.9
 #Go to project folder
 cd $HOME/luna16/src
 
+
 #Go!!!
-python learn.py
+THEANO_FLAGS='floatX=float32,device=gpu0,lib.cnmem=1' python learn.py
