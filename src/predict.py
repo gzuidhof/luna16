94343743b68d103027cf82d4fd77dc749ce7d700
Guido <me@guido.io>
2016-06-04 05:13:58 +0200

    Remove predictions outside of lungs

--- src/predict.py
+++ src/predict.py
@@ -1,20 +1,24 @@
+from __future__ import division
 import sys
 import params
-from params import params as P
 import numpy as np
 import os
 import skimage.io
 
+model_folder = '../models/'
+
+if len(sys.argv) < 2:
+    print "Missing arguments, first argument is model name, second is epoch"
+    quit()
+
+model_folder = os.path.join(model_folder, sys.argv[1])
+
+#Overwrite params, ugly hack for now
+params.params = params.Params(['../config/default.ini'] + [os.path.join(model_folder, 'config.ini')])
+from params import params as P
+
+
 if __name__ == "__main__":
-    model_folder = '../models/'
-
-    if len(sys.argv) < 2:
-        print "Missing arguments, first argument is model name, second is epoch"
-        quit()
-
-    model_folder = os.path.join(model_folder, sys.argv[1])
-    P = params.Params(['../config/default.ini'] + [os.path.join(model_folder, 'config.ini')])
-
     import theano
     import theano.tensor as T
     import lasagne
@@ -46,9 +50,9 @@ if __name__ == "__main__":
     predict_fn = unet.define_predict(network, input_var)
 
     in_pattern = '../data/1_1_1mm_512_x_512_lung_slices/subset9/*.pkl.gz'
-    filenames = glob(in_pattern)[:100]
+    filenames = glob(in_pattern)#[:100]
 
-    batch_size = P.BATCH_SIZE_VALIDATION
+    batch_size = 18
     multiprocess = False
 
     gen = ParallelBatchIterator(partial(load_images,deterministic=True),
@@ -59,8 +63,12 @@ if __name__ == "__main__":
     predictions_folder = os.path.join(model_folder, 'predictions_epoch{}'.format(epoch))
     util.make_dir_if_not_present(predictions_folder)
 
+    print "Disabling warnings (saving empty images will warn user otherwise)"
+    import warnings
+    warnings.filterwarnings("ignore")
+
     for i, batch in enumerate(tqdm(gen)):
-        inputs, _, _, filenames = batch
+        inputs, _, weights, filenames = batch
         predictions = predict_fn(inputs)[0]
 
         for n, filename in enumerate(filenames):
@@ -72,6 +80,10 @@ if __name__ == "__main__":
             f = os.path.join(predictions_folder,f+'.png')
 
             image = predictions[n*image_size:(n+1)*image_size][:,1].reshape(OUTPUT_SIZE,OUTPUT_SIZE)
-            image = np.array(image*255, dtype=np.uint8)
+
+            #Remove parts outside a few pixels from the lungs
+            image = image * np.where(weights[n,0,:,:]==0,0,1)
+
+            image = np.array(np.round(image*255), dtype=np.uint8)
 
             skimage.io.imsave(f, image)
