9c197fad27359137bcbe7d1e9cefc1bdf32d898d
Guido <me@guido.io>
2016-06-11 06:11:13 +0200

    Cluster scripts

--- scripts/default_experiment.sh
+++ scripts/default_experiment.sh
@@ -16,4 +16,4 @@ cd $HOME/luna16/src/deep
 
 echo "starting python"
 export THEANO_FLAGS='mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1' 
-srun -u python train.py ../../config/default_cluster.ini
+srun -u python train.py ../../config/default_05mm.ini ../../config/default_cluster.ini
