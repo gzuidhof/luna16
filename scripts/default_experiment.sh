e9e0643edb63c06bac0f10179da71623177b6494
Guido <me@guido.io>
2016-06-11 03:43:45 +0200

    Do not request cpu's on srun

--- scripts/default_experiment.sh
+++ scripts/default_experiment.sh
@@ -17,4 +17,4 @@ cd $HOME/luna16/src/deep
 
 echo "starting python"
 export THEANO_FLAGS='mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1' 
-srun -c 8 -u python train.py ../../config/default_cluster.ini
+srun -u python train.py ../../config/default_cluster.ini
