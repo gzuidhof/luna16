a8c42607784161ab07f5f14df57c2b7c17719f38
Guido <me@guido.io>
2016-06-20 17:26:14 +0200

    Subset to filenames dict

---
+++ src/subset.py
@@ -0,0 +1,21 @@
+import glob
+from sklearn.externals import joblib
+
+def make_filename_to_subset_dict():
+
+    in_folder = "/scratch-shared/vdgugten/cad/original/"
+    subset_dict = {}
+
+    for subset in range(10):
+        files = glob.glob(in_folder+'subset{0}'.format(subset)+'/*.mhd')
+        print "Subset", subset, "n files", len(files)
+        files = map(lambda x: x.replace('.mhd',''), files)
+        subset_dict[subset] = files
+
+    joblib.dump(subset_dict, '../config/subset_to_filenames.pkl')
+
+def get_filename_to_subset_dict(location='../config/subset_to_filenames.pkl'):
+    return joblib.load(location)
+
+if __name__ == "__main__":
+    make_filename_to_subset_dict()
