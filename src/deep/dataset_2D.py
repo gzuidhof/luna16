932b5a4cb90eac30bb3b4b028141a8c6197c8981
Wojova <jeroenmanders89@gmail.com>
2016-06-20 12:43:46 +0200

    added normalization to image loading

--- src/deep/dataset_2D.py
+++ src/deep/dataset_2D.py
@@ -1,17 +1,20 @@
 import gzip
 import cPickle as pickle
 import numpy as np
+from normalize import normalize
 
 from params import params as P
 
 def load_images (image_paths):
     X = []
 
-    for image_path in image_paths
-        with gzip.open(image_path):
-        X.append(pickle.load(file))
+    for image_path in image_paths:
+        with gzip.open(image_path) as file:
+        	X.append(pickle.load(file))
 
     X = np.array(X)
+
+    X = normalize(X)
     
     if P.ZERO_CENTER:
         X -= P.MEAN_PIXEL
