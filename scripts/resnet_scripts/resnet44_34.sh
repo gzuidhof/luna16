0784d67cb66756a8881abf475df38d031a17793e
Guido <me@guido.io>
2016-06-22 01:52:10 +0200

    Resnet splits

--- scripts/resnet_scripts/resnet44_34.sh
+++ scripts/resnet_scripts/resnet44_34.sh
@@ -13,9 +13,8 @@ module load cudnn
 #Go to project folder
 cd $HOME/luna16/src/deep
 
-
 #Go!!!
 
 echo "starting python"
-export THEANO_FLAGS='mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1' 
-srun -u python train.py ../../config/default_fr3dnet.ini
+export THEANO_FLAGS='mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1'
+srun -u python train.py ../../config/resnet44_34.ini
