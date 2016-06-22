1efc62d0347b9d13df69ce7675f375f82d6af04d
robbertvdg <robbert.gugten@student.ru.nl>
2016-06-22 20:55:32 +0200

    cartesius predict scripts

---
+++ scripts/resnet_predict/predict12.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -t 1-00:00:00
+#SBATCH -p gpu
+#SBATCH -c 12
+
+#Prepare python environment
+export PYTHONPATH=$HOME/pythonpackages/lib/python:$PYTHONPATH
+export PYTHONPATH=$HOME/pythonpackages/lib/python2.7/site-packages:$PYTHONPATH
+module load python/2.7.9
+module load cuda
+module load cudnn
+
+#Go to project folder
+cd $HOME/luna16/src/deep
+
+#Go!!!
+
+echo "starting python"
+export THEANO_FLAGS='mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1'
+srun -u python predict.py 1466562267_OWN_resnet32_12 168 12
