b3e7fe28a0607adb6fd751655d170fcc7984336d
robbertvdg <robbert.gugten@student.ru.nl>
2016-06-23 03:21:41 +0200

    Round coordinates

--- src/ensembleSubmissions.py
+++ src/ensembleSubmissions.py
@@ -17,7 +17,9 @@ def mergeFiles(submission_path):
 
 	# take the first submission file and set this in a dataFrame
 	mergedSubmission = pd.read_csv(submission_paths[0])
-
+	mergedSubmission['coordX'] = mergedSubmission['coordX'].round(2)
+	mergedSubmission['coordY'] = mergedSubmission['coordY'].round(2)
+	mergedSubmission['coordZ'] = mergedSubmission['coordZ'].round(2)
 	# remove the index column form the submission file
 	mergedSubmission.drop(mergedSubmission.columns[[0]], axis = 1, inplace = True)
 
@@ -25,12 +27,16 @@ def mergeFiles(submission_path):
 	for index in range(1, len(submission_paths)):
 		# create dataframe of submission file
 		submission = pd.read_csv(submission_paths[index])
+		submission['coordX'] = submission['coordX'].round(2)
+		submission['coordY'] = submission['coordY'].round(2)
+		submission['coordZ'] = submission['coordZ'].round(2)
 		# remove the index column form the submission file
 		submission.drop(submission.columns[[0]], axis = 1, inplace = True)
 		# merge rows which have the same values in the colomns: seriesuid, coordX, coordY, and coordZ
 		mergedSubmission = pd.merge(mergedSubmission, submission, on=['seriesuid', 'coordX', 'coordY', 'coordZ'], how = 'outer')
 
 	# calculate the probabiliy of the merged rows and add this to the final data Frame
+	print mergedSubmission
 	#probability = mergedSubmission[mergedSubmission.columns[range(4, 4 + len(submission_paths))]].mean(axis = 1)
 	#mergedSubmission.drop(mergedSubmission.columns[range(4, 4 + len(submission_paths))], axis = 1, inplace = True)
 	probability = mergedSubmission[mergedSubmission.columns[slice(4, None)]].mean(axis = 1)
