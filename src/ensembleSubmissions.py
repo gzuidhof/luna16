52456122cc78a161566092265c9635534236a830
Guido <me@guido.io>
2016-06-25 12:25:10 +0200

    Ensembling prediction equalization, filter distance 30mm

--- src/ensembleSubmissions.py
+++ src/ensembleSubmissions.py
@@ -1,6 +1,61 @@
+from __future__ import division
 import glob
 import pandas as pd
 import tqdm
+import matplotlib.pyplot as plt
+from numpy import *
+import numpy as np
+from skimage import exposure
+
+def histeq(im,nbr_bins=20000):
+
+   #get image histogram
+   imhist,bins = histogram(im,nbr_bins,normed=True)
+   cdf = imhist.cumsum() #cumulative distribution function
+   cdf = (nbr_bins-1)* cdf / cdf[-1] #normalize
+
+   #use linear interpolation of cdf to find new pixel values
+   im2 = interp(im,bins[:-1],cdf)
+
+   return im2#, cdf
+
+def histnorm(im,nbr_bins=2000):
+
+ #get image histogram
+ imhist,bins = histogram(im,nbr_bins,density=True)
+ #cdf = imhist.cumsum() #cumulative distribution function
+ #cdf = (nbr_bins-1)* cdf / cdf[-1] #normalize
+ cdf = imhist
+
+ #use linear interpolation of cdf to find new pixel values
+ im2 = interp(im,bins[:-1],cdf)
+
+ return im2#, cdf
+
+
+def hist_stretch(im, percentiles=(1, 99)):
+	p2, p98 = np.percentile(im, percentiles)
+	im = im *100000
+	#im = np.array(im, np.int64)
+
+	return exposure.rescale_intensity(im, in_range=percentiles)
+
+
+
+def equalize(df):
+	df = df.fillna(0)
+
+	#df[df['probability']<0.4] = 0
+
+	relevant = df[df['probability']>=0.4]
+	#print relevant
+	#print relevant['probability'].values
+	#relevant = histeq(relevant['probability'].values,len(relevant['probability'].values))
+	#print relevant['probability'].values.mean()
+	plt.hist(df[df['probability']>0.9]['probability'], 60)
+	plt.show()
+	return df
+
 
 # !!!!!!!!!!!!!!!!!!!!!!  IMPORTANT  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # this code is made for submission files, which have a index column.
@@ -23,6 +78,9 @@ def mergeFiles(submission_path):
 	# remove the index column form the submission file
 	mergedSubmission.drop(mergedSubmission.columns[[0]], axis = 1, inplace = True)
 
+	mergedSubmission = equalize(mergedSubmission)
+	#mergedSubmission['probability'] = histeq(mergedSubmission['probability'].values)
+
 	# loop over all submission files in the folder 'submission' and add them to the dataframe, except form the first one
 	for index in tqdm.tqdm(range(1, len(submission_paths))):
 		# create dataframe of submission file
@@ -32,6 +90,11 @@ def mergeFiles(submission_path):
 		submission['coordZ'] = submission['coordZ'].round(2)
 		# remove the index column form the submission file
 		submission.drop(submission.columns[[0]], axis = 1, inplace = True)
+
+
+
+		submission = equalize(submission)
+
 		# merge rows which have the same values in the colomns: seriesuid, coordX, coordY, and coordZ
 		mergedSubmission = pd.merge(mergedSubmission, submission, on=['seriesuid', 'coordX', 'coordY', 'coordZ'], how = 'outer')
 
@@ -48,8 +111,9 @@ def mergeFiles(submission_path):
 
 	mergedSubmission = mergedSubmission.fillna(0)
 
-	print mergedSubmission['probability'].count()
-	print len(mergedSubmission['probability'])
+
+	#plt.hist(mergedSubmission['probability'].values, 20000)
+	#plt.show()
 
 	return mergedSubmission
 
