18af08408d6a7aafccc9affe6f93da6e7a22724d
Guido <me@guido.io>
2016-06-26 02:46:50 +0200

    Add resnet56 scripts

---
+++ scripts/resnet_scripts/resnet56_34.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -t 2-00:00:00
+#SBATCH -p gpu
+#SBATCH -c 12
+
+#Prepare python environment
+export PYTHONPATH=$HOME/pythonpackages/lib/python:$PYTHONPATH
+export PYTHONPATH=$HOME/pythonpackages/lib/python2.7/site-packages:$PYTHONPATH
+module load python/2.7.9
+module load cuda
+module load cudnn
+
+#Go to project folder
+cd $HOME/luna16/src/deep
+
+#Go!!!
+
+echo "starting python"
+export THEANO_FLAGS='mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1'
+srun -u python train.py ../../config/resnet56_34.ini
